585d379439df4fba9d169bd722be2497a24349aa
MaanooAk <akritasak@gmail.com>
2018-05-12 13:17:05 +0300

    Basic template app files

---
+++ app/src/main/java/com/csdfossteam/palermo/face/MainActivity.java
@@ -0,0 +1,17 @@
+package com.csdfossteam.palermo.face;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.csdfossteam.palermo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    // this is the template main activity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
