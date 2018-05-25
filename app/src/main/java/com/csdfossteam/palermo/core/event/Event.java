961563cfe1c101082691efa5071b3ce407416da2
MaanooAk <akritasak@gmail.com>
2018-05-25 13:26:35 +0300

    Events first draft

--- app/src/main/java/com/csdfossteam/palermo/core/event/Event.java
+++ app/src/main/java/com/csdfossteam/palermo/core/event/Event.java
@@ -1,10 +1,32 @@
 package com.csdfossteam.palermo.core.event;
 
-/**
- * TODO doc
- *
- * @author MaanooAk
- */
+import com.csdfossteam.palermo.core.Game;
 
-public class Event {
+import java.io.Serializable;
+
+/**
+ * An abstraction of an event.
+ *
+ * @author Akritas Akritidis
+ */
+public abstract class Event implements Serializable {
+
+    private final String id;
+
+    private final String message; // TODO move elsewhere
+    private final Object[] messageArgs;
+
+    public Event(String id, String message, Object... messageArgs) {
+        this.id = id;
+
+        this.message = message;
+        this.messageArgs = messageArgs;
+    }
+
+    public abstract void apply(Game game);
+
+    public String message() {
+        return String.format(message, messageArgs);
+    }
+
 }
--- app/src/main/java/com/csdfossteam/palermo/core/event/Events.java
+++ app/src/main/java/com/csdfossteam/palermo/core/event/Events.java
@@ -1,10 +1,35 @@
 package com.csdfossteam.palermo.core.event;
 
+import com.csdfossteam.palermo.core.Game;
+import com.csdfossteam.palermo.core.Player;
+
+import java.io.Serializable;
+
 /**
  * TODO doc
  *
- * @author MaanooAk
+ * @author Akritas Akritidis
  */
+public final class Events {
 
-public class Events {
+    private Events() {}
+
+    public class PlayerVote extends Event {
+
+        public final Player.Id player;
+        public final Serializable vote;
+
+        public PlayerVote(String id, String message, Player.Id player, Serializable vote) {
+            super(id, message, player, vote);
+            this.player = player;
+            this.vote = vote;
+        }
+
+        @Override
+        public void apply(Game game) {
+
+            game.vote.set(player.get(game.players), vote);
+        }
+    }
+
 }
